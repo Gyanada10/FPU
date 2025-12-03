98019a22aced7a688151f7724ebea1d9f72abd3f
Ankitha-Gargya <ankithagargya2807@gmail.com>
2025-12-03 12:51:32 +0530

    Update ALU .v
    working_top

--- ALU .v
+++ ALU .v
@@ -1,10 +1,7 @@
-`include "Addition_Subtraction.v"
-`include "Multiplication.v"
-`include "Division.v"
 
 module ALU (
-  input  wire  clk,       
-  input  wire  reset,      
+         
+  input  wire  enable,      
   input  wire  [3:0]  Operation, // ALU operation code
   input  wire  [63:0] a_operand, // operand A
   input  wire  [63:0] b_operand, // operand B
@@ -27,6 +24,7 @@ module ALU (
              OP_RS = 4'd10,  //1 bit right shift on a_operand
              OP_FPI = 4'd11; //converting FP a_operand to integer
 
+  
   // 2. Safe input driving for submodules (no 'z')
   wire  add_sub_ctrl = (Operation == OP_SUB); // 1=sub, 0=add
 
@@ -37,24 +35,23 @@ module ALU (
 
   // 4. Submodule instantiations (with enable signals)
   wire [63:0] Add_Sub_Output, Mul_Output, Div_Output, Int_output;
-  wire Add_Sub_Exception, Mul_Exception, Div_Exception;
-  wire Mul_Overflow, Mul_Underflow;
-
+  wire Add_Sub_Exception, Add_Sub_Overflow, Add_Sub_Underflow ;
+  wire Mul_Exception,Mul_Overflow, Mul_Underflow;
+  wire Div_Exception, Div_Overflow, Div_Underflow;
 
   Addition_Subtraction addsub_inst (
-    .clk(clk),
     .enable(enable_addsub),             
     .a_operand(a_operand),
     .b_operand(b_operand),
     .Add_or_Sub(add_sub_ctrl),
+    .Result(Add_Sub_Output),
     .Exception(Add_Sub_Exception),
-    .Result(Add_Sub_Output)
+    .Overflow(Add_sub_Overflow),
+    .Underflow(Add_sub_Underfow)  
   );
 
   Multiplication mul_inst (
-    .clk(clk),
     .enable(enable_mul),
-    .reset(reset),
     .a_operand(a_operand),
     .b_operand(b_operand),
     .Exception(Mul_Exception),
@@ -63,49 +60,104 @@ module ALU (
     .result(Mul_Output)
   );
 
- Division div_inst (
-    .clk(clk),
-    .enable(enable_div),
-    .reset(reset),
-    .a(a_operand),
-    .b(b_operand),
-    .q(Div_Output),
-    .r(),
-    .busy(),
-    .ready(),
-    .count()
+Division divinst(
+	.enable(enable_div),
+    .a(a_operand),     
+    .b(b_operand),     
+    .Result(Div_Output), 
+    .Exception(Div_Exception) 
 );
 
 
-  // 5. Combinational logic for operation selection
-  reg [63:0] alu_out;
-  reg exc, ovf, unf;
+
 
   always @(*) begin
-    alu_out = 64'd0; exc = 1'b0; ovf = 1'b0; unf = 1'b0;
+  
+	if (enable) begin
+		
     case(Operation)
-      OP_ADD,
-      OP_SUB: begin alu_out = Add_Sub_Output; exc = Add_Sub_Exception; end
-      OP_MUL: begin alu_out = Mul_Output; exc = Mul_Exception;
-               ovf = Mul_Overflow; unf = Mul_Underflow; end
-      OP_DIV: begin alu_out = Div_Output; exc = Div_Exception; end
-      OP_AND: alu_out = a_operand & b_operand;
-      OP_OR : alu_out = a_operand | b_operand;
-      OP_XOR: alu_out = a_operand ^ b_operand;
-      OP_NOT: alu_out = ~a_operand;   
-      OP_LS: alu_out = a_operand << 1'b1;
-      OP_RS: alu_out = a_operand >> 1'b1;
-      OP_FPI: alu_out = Int_output;
-      default: alu_out = 64'd0;
-    endcase
+		
+      OP_ADD, OP_SUB: 
+			begin 
+				ALU_Output = Add_Sub_Output; 
+				Exception = Add_Sub_Exception;
+				Overflow = Add_sub_Overflow;  	
+				Underflow =Add_sub_Underfow;
+			end		
+			
+      OP_MUL: 
+			begin
+				ALU_Output = Mul_Output; 
+				Exception = Mul_Exception;
+				Overflow = Mul_Overflow; 
+				Underflow = Mul_Underflow;
+			end			
+			
+      OP_DIV: 
+			begin 
+				ALU_Output = Div_Output; 
+				Exception = Div_Exception;
+			end			
+			
+      OP_AND: 
+            begin
+			     ALU_Output = a_operand & b_operand;
+			     Exception = 1'b0;
+			end		
+		
+      OP_OR :
+            begin
+			     ALU_Output = a_operand | b_operand;
+			     Exception = 1'b0;
+			end	
+						
+			
+      OP_XOR: 
+			begin
+			     ALU_Output = a_operand ^ b_operand;
+			     Exception = 1'b0;
+			end			
+			
+      OP_NOT: 
+			begin
+			     ALU_Output = ~a_operand;
+			     Exception = 1'b0;
+			end	  		
+			
+      OP_LS: 	
+			begin
+			     ALU_Output = a_operand << 1'b1;	
+			     Exception = 1'b0;
+			end	
+			
+      OP_RS: 
+			begin
+			     ALU_Output = a_operand >> 1'b1;	
+			     Exception = 1'b0;
+			end	
+				
+      OP_FPI: 
+			begin
+			     ALU_Output = Int_output;	
+			     Exception = 1'b0;
+			end	
+					
+			
+      default: ALU_Output = 64'd0;
+    
+	endcase
   end
-
-  // 6. Registered outputs (timing stability + glitch reduction)
-  always @(posedge clk) begin
-    ALU_Output <= alu_out;
-    Exception  <= exc;
-    Overflow   <= ovf;
-    Underflow  <= unf;
+    
+  else begin
+  
+		  ALU_Output = 64'd0; 
+		  Exception = 1'b0; 
+		  Overflow = 1'b0; 
+		  Underflow = 1'b0;
+  
+  end
+  
   end
 
 endmodule
+
